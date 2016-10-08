c73d3a1bbd52462d1c2d3f9d447589eb4948cc3e
Iede Snoek <snoekiede@gmail.com>
2016-10-08 17:26:51 +0200

    Added the first classes

---
+++ ElasticSearch/ElasticSearch/Contexts/ElasticSearchContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticSearch.Contexts
+{
+    
+    class ElasticSearchContext
+    {
+        public String Url { get; private set; }
+
+        public ElasticSearchContext(String url)
+        {
+            Url = url;
+        }
+
+
+
+
+    }
+}
