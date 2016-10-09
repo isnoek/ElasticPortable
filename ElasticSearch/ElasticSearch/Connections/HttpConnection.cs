f5c5cb1b795e3074cedd0c332e92869e53fe8d11
Iede Snoek <snoekiede@gmail.com>
2016-10-09 09:10:37 +0200

    Added a real unit test project

--- ElasticSearch/ElasticSearch/Connections/HttpConnection.cs
+++ ElasticSearch/ElasticSearch/Connections/HttpConnection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,22 @@ namespace ElasticSearch.Connections
 {
     public class HttpConnection
     {
+        private String serverUri;
+        public HttpConnection(String elasticServerUri)
+        {
+            serverUri = elasticServerUri;
+        }
+
+        public async Task<String> performQuery(String url,HttpContent content)
+        {
+            var baseUrl = serverUri;
+            if (!baseUrl.EndsWith("/"))
+            {
+                baseUrl = baseUrl + "/";
+            }
+            baseUrl = baseUrl + url;
+            var message= await new HttpClient().PostAsync(baseUrl, content);
+            return message.ToString();
+        }
     }
 }
